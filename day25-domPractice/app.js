79550c83d75f0a5800aa5e06a38471d2e230413d
Ganesh Gaikwad <theganeshgaikwad@gmail.com>
2025-09-25 13:57:21 +0530

    learned dom theory and take notes

---
+++ day25-domPractice/app.js
@@ -0,0 +1,14 @@
+// DOM Practice - 25 Sept 2025
+
+// Selecting elements
+// let heading = document.getElementById("title");
+
+// Manipulating content
+// heading.innerText = "Hello DOM!";
+
+// Adding element dynamically
+// let newDiv = document.createElement("div");
+// newDiv.innerText = "New Element added";
+// document.body.appendChild(newDiv);
+
+// TODO: Practice DOM mini-projects (to-do app, color changer, slider)
